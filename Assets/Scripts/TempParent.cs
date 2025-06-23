f291b991711bb12f6afb47517ac50ab910bf2294
coderic <76266284+cod-eric@users.noreply.github.com>
2025-06-23 18:02:24 +0200

    added throwable, but compiler errors in orig. code

---
+++ Assets/Scripts/TempParent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TempParent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static TempParent Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
